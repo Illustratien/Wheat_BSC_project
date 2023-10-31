1a97c38e673d9dd15e8ae6c166f5fb2e4aa90172
HU-Studienprojekt-2023Winter <acikgoee@hu-berlin.de>
2023-10-31 12:02:37 +0100

    update with comments

--- src/Week2.R
+++ src/Week2.R
@@ -1,3 +1,7 @@
+# copy --------------------------------------------------------------------
+# ctr+shift+r
+# block it in ctr,shift+c
+
 # check if pattern exist in vector
 3%in%c(1,3) 
 2%in%c(1,3) 
@@ -8,10 +12,13 @@
 c(1,3)==2
 # which zeigt an, an welcher position wo die richtige aussage ist
 
+# self function -----------------------------------------------------------
+
+
 reihenfolge <- function(vec,dec=TRUE){
   #numeric,char vec ist alles was c() ist
   #diese function setzt alle elemente des vektors in einer ausgewählten reiehenfolge
-vec[order(vec,decreasing= dec)]
+  vec[order(vec,decreasing= dec)]
 }
 
 order(c(3,1,2))
@@ -19,14 +26,17 @@ order(c(3,1,2),decreasing = TRUE)
 c(3,1,2)[order(c(3,1,2))]
 which(c(1,3)==3) 
 reihenfolge(c("a","b","c"),dec=FALSE
-          ) 
+) 
 reihenfolge(c(3,1,2,5,4,5,7,8),dec = FALSE) 
 # what will be the difference?
 #order macht, zeigt, wo man die angegeben vectoren angeben muss, damit man die richtige reiehenfolge kriegt
 order(c(3,1,2)) 
 c(3,1,2) %>% .[order(.)]
 #[] ist für die auswahl eines elemts des vektors zb richtige psoition von line 15
-F
+
+# -------------------------------------------------------------------------
+
+
 # what will be the data type? check with str()
 c(1,2,NA) %>% is.na() 
 c(1,2,NA) %>% is.na() %>% which() 
