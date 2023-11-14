46b09e5058b77db83e3667982268a8a82b93b7c7
HU-Studienprojekt-2023Winter <acikgoee@hu-berlin.de>
2023-11-14 12:12:04 +0100

    update

--- src/Week1.R
+++ src/Week1.R
@@ -15,15 +15,27 @@ seq(date_element,by=1, length.out=6)%>% mean(.)
 vec1<-c("a","b")
 vec2<-c("1","2")
 vec1
-vec2 
-rep(vec1, each=2)
-rep(vec2, times=2)
-rep("vec1"+"vec2",each=2,times=2)
-paste(rep("vec1"+"vec2",each=2,times=2))
+vec2
+'vec2'
+# sumvec<-str
+  vec1+vec2
+#match(c("s","c"), letters)
+wiederholungalphabet<-rep(vec1, each=2)
+wiederholungziffern<-rep(vec2, times=2)
+
+# rep(vec1,vec2,each=2,times=2)
+# syntax paste(x,..., sep="")
+# paste(rep("vec1"+"vec2",each=2,times=2))
+#so funktioniert das zeile 30
+paste(wiederholungalphabet,
+      wiederholungziffern,
+      sep = "")
 vec1_numeric<- as.numeric(vec1)
 rep(vec1_numeric+vec2,each=2,times=2)
 vec_sum<- vec1_numeric+vec2
 suppressWarnings(as.numeric(vec1))                    
 vec1.1<- as.numeric(vec1)
-result <- paste(rep(vec1, each = length(vec2)), rep(vec2, times = length(vec1)))
+result <- paste(rep(vec1, each = length(vec2)), 
+                rep(vec2, times = length(vec1)),
+                sep="")
 print(result)
---
+++ src/Week4.R
@@ -0,0 +1,43 @@
+# input is vector
+library(dplyr)
+c(1,4) %>% 
+  lapply(.,FUN=function(x){x+3})
+# input is list
+list(2,4,c(1,4)) %>% 
+  lapply(.,FUN=function(x){x+3})
+# input has differnt type
+list(2,4,c(1,4),"8") %>% 
+  lapply(.,FUN=function(x){x+3})
+
+# create a dataframe 
+df <- data.frame(
+  time=as.Date("2023-04-16",format="%Y-%m-%d")+seq(1,3,1),
+  temp=c(20,15,13),
+  thermal_time=cumsum(c(20,15,13))
+)
+data.frame(
+  time=as.Date("2023-04-16",format="%Y-%m-%d")+seq(1,3,1),
+  temp=c(20,15),
+  thermal_time=cumsum(c(20,15,13))
+)
+# another way
+df <- data.frame(time=as.Date("2023-04-16",format="%Y-%m-%d")+seq(1,3,1)) 
+df$temp=c(20,15,13)
+df$thermal_time=cumsum(df$temp)
+
+# third method
+library(dplyr)
+df <- data.frame(time=as.Date("2023-04-16",format="%Y-%m-%d")+seq(1,3,1)) %>% 
+  mutate(temp=c(20,15,13), 
+         thermal_time=cumsum(temp))
+#challange week 2 df
+df
+
+vectthermaltime<-df$"thermal_time"
+vectthermaltime
+vectthermaltime
+df[time(),]
+str(2023-04-17)
+str(vec)
+"2023-04-17"==df[,1]
+df["2023-04-17"==df[,1],2]
